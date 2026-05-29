33e9d5e651f82844d56640135b3de924f7d62069
anandm032 <anandm15032005@gmail.com>
2026-05-29 12:28:05 +0530

    Update student_management.py

--- student_management.py
+++ student_management.py
@@ -12,7 +12,21 @@ class Student:
         print("Name:", self.name)
         print("Roll No:", self.rollno)
         print("Marks:", self.marks)
-
+def load_students():
+    file = open("student.txt","r")
+    for line in file:
+        name,rollno,marks = line.strip().split(",")
+        student_ob = Student(name,int(rollno),int(marks))
+        students_list.append(student_ob)
+    file.close()
+        
+def save_students():
+        file = open("student.txt", "w")
+        for stun in students_list:
+            file.write(f"{stun.name},{stun.rollno},{stun.marks}\n")
+        file.close()
+        
+load_students()
 
 while True:
     print("                ")
@@ -21,7 +35,8 @@ while True:
     print("3= SEARCH THE ROLL NO")
     print("4= DELETE THE STUDENT")
     print("5= update the mark")
-    print("6 = EXIT")
+    print("6= SAVE THE DETAILS")
+    print("7 = EXIT")
     
     choise = int(input("enter the choice"))
     
@@ -73,11 +88,12 @@ while True:
                 print("Student MARK UPDATED successfully")
         if found== False:
                  print("Student not found")
-                 
-       
-            
-            
     elif choise==6:
+        save_students()
+        print("students saved sucessfully")
+                   
+            
+    elif choise==7:
         break
     else:
         print("input a valid choice")
